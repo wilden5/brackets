db3feff8b4b2cf334952e3c38bad0d66fb0155ee
Denis Ivanchenko <sicknature24@yandex.com>
2023-02-23 18:59:32 +0300

    feat: add part of solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let curlyOpen = [];
+    let curlyClosed = [];
+    let squareOpen = [];
+    let squareClosed = [];
+    let thirdOpen = [];
+    let thirdClosed= [];
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === '('){
+            curlyOpen.push(str[i]);
+        } else if(str[i] === ')') {
+            curlyClosed.push(str[i]);
+        } else if (str[i] === '[') {
+            squareOpen.push(str[i]);
+        } else if (str[i] === ']') {
+            squareClosed.push(str[i]);
+        } else if (str[i] === '{') {
+            thirdOpen.push(str[i]);
+        } else if (str[i] === '}') {
+            thirdClosed.push(str[i]);
+        }
+    }
+    if (curlyOpen.length > 0 && curlyOpen.length === curlyClosed.length) {
+        return true;
+    } else if (squareOpen.length > 0 && curlyOpen.length === squareClosed.length) {
+        return true;
+    } else if (thirdOpen.length > 0 && curlyOpen.length === thirdClosed.length) {
+        return true;
+    } else {
+        return false;
+    }
 }
